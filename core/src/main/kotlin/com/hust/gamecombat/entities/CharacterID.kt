5240cb78cbc2dc9467cf2dd7381c8217b67f05b2
Duong <155739353+duongdh710@users.noreply.github.com>
2025-10-19 18:05:41 +0700

    add CharacterID

---
+++ core/src/main/kotlin/com/hust/gamecombat/entities/CharacterID.kt
@@ -0,0 +1,6 @@
+package com.hust.gamecombat.entities
+
+enum class CharacterID(val assetPath: String, val displayName: String) {
+    NARUTO("characters/naruto", "Naruto"),
+    LUFFY("characters/luffy", "Luffy");
+}
